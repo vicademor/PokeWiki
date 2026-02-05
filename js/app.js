07da4e3cd64c32a67299b207eb5c10b112a06e8a
vicademor <victoradelantado01@gmail.com>
2026-02-05 18:52:55 +0100

    cambio de icono y ajustes necesarios en los html

--- js/app.js
+++ js/app.js
@@ -5,3 +5,23 @@ toggleBtn.addEventListener('click', () => {
   document.body.classList.toggle('sidebar-hidden');
   document.querySelector('header').classList.toggle('sidebar-hidden');
 });
+
+const targetsDarkTheme = document.querySelectorAll('.texto-gen, h2');
+const targetsDarkElement = document.querySelectorAll('.pokeball-nav, .titulos');
+
+document.getElementById('themeToggle').addEventListener('click', () => {
+    document.body.classList.toggle("dark-theme");
+    targetsDarkTheme.forEach(el => el.classList.toggle("dark-theme"));
+    targetsDarkElement.forEach(el => el.classList.toggle("dark-element"));
+
+    const isDark = document.body.classList.contains("dark-theme");
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+});
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.body.classList.add("dark-theme");
+    targetsDarkTheme.forEach(el => el.classList.add("dark-theme"));
+    targetsDarkElement.forEach(el => el.classList.add("dark-element"));
+}
